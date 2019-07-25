3046a0ecc14578a4d6c0031da32495f6d9b1454d
vinicius fonseca <viniciusfon96@gmail.com>
2019-07-24 22:00:04 -0300

    conversão dos nomes de editais para lowerCase

--- index.js
+++ index.js
@@ -16,16 +16,14 @@ var arquivos = [];
 crawler.crawl(
     "https://fapergs.rs.gov.br/mapa-do-site",
     function onSuccess(page) {
-        if (page.url.indexOf("/edital-") > 0) {
+        if (page.url.toLowerCase().indexOf("/edital-") > 0) {
             editais.push(page.url);
-           
-        }
-        if (page.url.indexOf(".pdf".toLowerCase()) > 0) {
-            arquivos.push({url: page.url.toLowerCase(), ref: page.referer});
-           
-        }
 
+        }
+        if (page.url.toLowerCase().indexOf(".pdf") > 0) {
+            arquivos.push({ url: page.url, ref: page.referer });
 
+        }
 
     },
     null,
@@ -34,31 +32,26 @@ crawler.crawl(
         console.log('All crawling finished');
 
         arquivos.map(item => {
-         
-            if(item.url.indexOf(".pdf".toLowerCase()) && item.ref.indexOf("/edital-" ) >0) {
-            console.log("alo");
-            const https = require('https');
-            const fs = require('fs');
 
-            if (!fs.existsSync("./editais/" + item.ref.slice(26))) {
-                fs.mkdirSync("./editais/" + item.ref.slice(26));
-            } else {
-                console.log("ja existe " + item.ref.slice(26))
-            }
+            if (item.url.indexOf(".pdf") && item.ref.indexOf("/edital-") > 0) {
+                console.log("alo");
+                const https = require('https');
+                const fs = require('fs');
 
-            const file = fs.createWriteStream("./editais/" + item.ref.slice(26) + "/" + item.url.slice(58));
-            const request = https.get(page.url, function (response) {
-                response.pipe(file);
+                if (!fs.existsSync("./editais/" + item.ref.slice(26))) {
+                    fs.mkdirSync("./editais/" + item.ref.slice(26));
+                } else {
+                    console.log("ja existe " + item.ref.slice(26))
+                }
 
-            });
+                const file = fs.createWriteStream("./editais/" + item.ref.slice(26) + "/" + item.url.slice(58));
+                const request = https.get(page.url, function (response) {
+                    response.pipe(file);
 
+                });
 
-        
+            };
 
-        };
-
-
-
-    })
+        })
 
     });
