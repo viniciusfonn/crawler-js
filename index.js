9b0a65a60fd2521382bc4395dd8e351af0a2d3aa
Alice Quadros <alicequadros63@gmail.com>
2019-07-24 21:36:18 -0300

    correção de problema de tim timeout

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var Crawler = require("js-crawler");
- 
+
 var crawler = new Crawler().configure({
-    depth: 2,
+    depth: 3,
     shouldCrawl: function (url) {
         return url.indexOf("fapergs.rs.gov.br") > 0;
     },
@@ -9,71 +9,56 @@ var crawler = new Crawler().configure({
         return url.indexOf("fapergs.rs.gov.br") > 0;
     }
 });
- 
+
 var editais = [];
 var arquivos = [];
- 
+
 crawler.crawl(
     "https://fapergs.rs.gov.br/mapa-do-site",
     function onSuccess(page) {
         if (page.url.indexOf("/edital-") > 0) {
             editais.push(page.url);
-            
- 
+           
         }
+        if (page.url.indexOf(".pdf".toLowerCase()) > 0) {
+            arquivos.push({url: page.url.toLowerCase(), ref: page.referer});
+           
+        }
+
+
+
     },
     null,
-    function onAllFinished(crawledUrls) {
-        console.log('All crawling finished');
-        console.log(editais);
- 
-        editais.map(item => {
- 
-            var crawler2 = new Crawler().configure({
-                depth: 2,
-                shouldCrawl: function (url) {
-                    return url.indexOf("fapergs.rs.gov.br") > 0;
-                },
-                shouldCrawlLinksFrom: function (url) {
-                    return url.indexOf("fapergs.rs.gov.br") > 0;
-                }
-            });
- 
-            crawler2.crawl(
-                item,
-                function onSuccess(page, url) {
-                    if (page.url.indexOf(".pdf") > 0) {
-                        arquivos.push(page.url);
- 
-                        const https = require('https');
-                        const fs = require('fs');
- 
-                        if (!fs.existsSync("./editais/" + item.slice(26))) {
-                            fs.mkdirSync("./editais/" + item.slice(26));
-                        }  else {
-                        console.log("ja existe " + item.slice(26))
-                        }
-                       
-                        const file = fs.createWriteStream("./editais/" + item.slice(26) + "/" + page.url.slice(49));
-                        const request = https.get(page.url, function (response) {
-                            response.pipe(file);
-                       
-                        });
- 
-                    }
-                },
-                null,
-                function onAllFinished(crawledUrls) {
-                    
-                });
- 
-        })
- 
-    }
- 
-)
- 
- 
 
- 
- 
+    function onAllFinished(page, url) {
+        console.log('All crawling finished');
+
+        arquivos.map(item => {
+         
+            if(item.url.indexOf(".pdf".toLowerCase()) && item.ref.indexOf("/edital-" ) >0) {
+            console.log("alo");
+            const https = require('https');
+            const fs = require('fs');
+
+            if (!fs.existsSync("./editais/" + item.ref.slice(26))) {
+                fs.mkdirSync("./editais/" + item.ref.slice(26));
+            } else {
+                console.log("ja existe " + item.ref.slice(26))
+            }
+
+            const file = fs.createWriteStream("./editais/" + item.ref.slice(26) + "/" + item.url.slice(58));
+            const request = https.get(page.url, function (response) {
+                response.pipe(file);
+
+            });
+
+
+        
+
+        };
+
+
+
+    })
+
+    });
