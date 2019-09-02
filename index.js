61539cf7120327a3812d07ac69d4b18f61e0866b
Alice Quadros <alicequadros63@gmail.com>
2019-09-02 20:04:19 -0300

    separacao dos arquivos

--- index.js
+++ index.js
@@ -1,49 +1,23 @@
 const Crawler = require("js-crawler");
 
-const https = require('https');
-const fs = require('fs');
+import leitura from "./leitura.js";
+import download from "./download.js";
+
+const arquivos = [];
 
 const crawler = new Crawler().configure({
     depth: 3,
-    shouldCrawl: url => url.indexOf("fapergs.rs.gov.br") > 0,
+    shouldCrawl: url => url.indexOf("fapergs.rs.gov.br") > 0
 });
-
-const arquivos = [];
 
 crawler.crawl(
     "https://fapergs.rs.gov.br/mapa-do-site",
     function onSuccess(page) {
-
-        if (page.url.toLowerCase().indexOf(".pdf") > 0 && page.referer.indexOf("/edital-") > 0) {
-            arquivos.push({ url: page.url, ref: page.referer });
-        }
-
+        const arquivo = (download(page));
+        arquivos.push(arquivo);
     },
-    function onFailure() {
-        console.log("erro ao fazer o crawler")
-    },
-    function onAllFinished(page) {
-
-        if (!fs.existsSync("./editais/")) {
-            fs.mkdirSync("./editais/");
-        }
-
-        arquivos.map(item => {
-
-            if (!fs.existsSync(`./editais/${item.ref.slice(26)}`)) {
-                fs.mkdirSync(`./editais/${item.ref.slice(26)}`);
-            }
-
-            const file = fs.createWriteStream(`./editais/${item.ref.slice(26)}/${item.url.slice(58)}`);
-
-            if (!item.url.indexOf("127.0.0.1") > 0) {
-                https.get(page.url, response => response.pipe(file)).on("error", err => console.log("Error: " + err.message));
-            }
-
-        }
+    null,
+    function onAllFinished() {
+        leitura(arquivos);
 
     });
-
-
-    
-
