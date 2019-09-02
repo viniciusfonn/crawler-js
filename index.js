213a78b7a8d50668b3adba68359df65f34946aaa
vinicius fonseca <viniciusfon96@gmail.com>
2019-09-02 20:07:16 -0300

    adicao da api

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const https = require('https');
 const fs = require('fs');
 const path = require('path');
 const extract = require('pdf-text-extract');
+const axios = require('axios');
+
+
+
 
 let arquivos = [];
 
@@ -24,7 +28,12 @@ crawler.crawl(
                 fs.mkdirSync(`./editais/${page.referer.slice(26)}`);
             }
             const file = fs.createWriteStream(`./editais/${page.referer.slice(26)}/${page.url.slice(58)}`);
-            https.get(page.url, response => response.pipe(file)).on("error", err => console.log(`Error: ${err.message}`));
+            https.get(page.url, response => response.pipe(file)).on("error", err => {
+                axios.post('http://localhost:3333/api/errors', {
+                    error: err
+                });
+            }
+            );
         }
     },
     null,
@@ -34,10 +43,17 @@ crawler.crawl(
             const filePath = path.join(__dirname, `./editais/${item.ref.slice(26)}/${item.url.slice(58)}`)
             extract(filePath, function (err, pages) {
                 if (err) {
-                    console.dir(err)
+                    axios.post('http://localhost:3333/api/errors', {
+                        error: err
+                    });
                     return
                 }
                 else {
+
+                    axios.post('http://localhost:3333/api/file', {
+                        pagina: pages
+                    });
+
                     fs.writeFile(`./editais/${item.ref.slice(26)}/${item.url.slice(58, -3)}.txt`, pages, function (erro) {
                         if (erro) {
                             throw erro;
